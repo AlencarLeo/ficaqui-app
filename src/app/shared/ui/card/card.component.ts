ea6cc97dfa4d883752981dbea34fb6775efc4943
Leonardo Alencar <alencar.leonardo2020@gmail.com>
2024-02-08 03:49:07 -0300

    add? pipe currency and mobile version

--- src/app/shared/ui/card/card.component.ts
+++ src/app/shared/ui/card/card.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
